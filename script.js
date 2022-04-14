3af69edb7f69b252cf614a950ba4670371b256b7
Josh S <aaabccc87@protonmail.com>
2022-04-13 21:17:47 -0400

    Wrote rockPaperScissors() function

--- script.js
+++ script.js
@@ -12,3 +12,41 @@ function computerPlay(){
             console.log("Error in computerPlay()");
     }
 }
+
+function rockPaperScissors(playerSelection, computerSelection){
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
+
+    if (playerSelection === computerSelection){
+        return "Draw! Both players chose " + computerSelection;
+    }
+
+    if (playerSelection === "rock"){
+        if (computerSelection === "paper"){
+            return "You lose! Paper beats rock!";
+        }
+        else if (computerSelection === "scissors"){
+            return "You win! Rock beats scissors!");
+        }
+    }
+
+    if (playerSelection === "paper"){
+        if (computerSelection === "rock"){
+            return "You win! Paper beats rock!";
+        }
+        else if (computerSelection === "scissors"){
+            return "You lose! Scissors beats paper!";
+        }
+    }
+
+    if (playerSelection === "scissors"){
+        if (computerSelection === "paper"){
+            return "You win! Scissors beats paper!";
+        }
+        else if (computerSelection === "rock"){
+            return "You lose! Rock beats scissors!");
+        }
+    }
+
+
+}
